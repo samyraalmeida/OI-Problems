3f814cdf01d8baa146358631b5d8e148f746c374
Samyra Almeida <samyraalmeida@alu.ufc.br>
2021-03-04 08:53:39 -0300

    Update pastas.cpp

--- OBI/OBI 2007/P2/Fase 1/pastas.cpp
+++ OBI/OBI 2007/P2/Fase 1/pastas.cpp
@@ -1,24 +1,31 @@
+/*
+    OBI 2007 - P2 - Fase 1 - Pastas
+    
+    Complexidade: O(min(P, N))
+    
+    Solução por Samyra Almeida
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int maxn = 1e6+10;
+const int maxn = 1e6 + 10;
 
-int n, p;
+int n, p, ct[maxn];
 bool ck[maxn];
-int ct[maxn];
 
 int main()
 {
 	cin >> p >> n;
+	
 	bool ok = false;
 
 	for(int i = 1 ; i <= n ; ++i)
 	{
-		int x;
-		cin >> x;
-		ck[x] = 1;
-		ct[x]++;
+		int x;  cin >> x;
+		
+		ck[x] = 1;  ct[x]++;
 	}
 
 	for(int i = 1 ; i <= min(p,n) ; ++i)
@@ -29,23 +36,15 @@ int main()
 			return 0;
 		}
 	}
-	//cout << "\n\n";
 
 	for(int i = 2 ; i <= min(p, n) ; ++i)
-	{	//cout << at << " "<< i << " " << ct[i] << "\n";
+	{
 		if(ct[i] != ct[i-1])
 		{
-			//cout << "oi krl\n";
-			if(ct[i] == ct[i-1] - 1 && ok == 0)
-			{
-				ok = 1;
-			}else{
-				cout << "N\n";
-				return 0;
-			}
+			if(ct[i] == ct[i-1] - 1 && ok == 0) { ok = 1; }
+			else { cout << "N\n";  return 0; }
 		}
 	}
-	cout << "S\n";
 
-	return 0;
+	cout << "S\n";  return 0;
 }
