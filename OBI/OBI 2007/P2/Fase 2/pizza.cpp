f5fbaec58a44d2cf8dfb5f09e2bbb4efb61e1d14
Samyra Almeida <samyraalmeida@alu.ufc.br>
2021-03-04 09:00:09 -0300

    Update pizza.cpp

--- OBI/OBI 2007/P2/Fase 2/pizza.cpp
+++ OBI/OBI 2007/P2/Fase 2/pizza.cpp
@@ -11,32 +11,31 @@
 
 using namespace std;
 
-const int MAXN=2e5+10;
+const int maxn = 2e5 + 10;
 
-int n, ans, b[MAXN], s[MAXN], v[MAXN];
+int n, ans, b[maxn], s[maxn], v[maxn];
 
-int main(){
-
+int main()
+{
 	scanf("%d", &n);
 
-	for(int i=0;i<n;++i){
-		scanf("%d", &v[i]);
+	for(int i = 0 ; i < n ; ++i) { scanf("%d", &v[i]); }
+
+	for(int i = 0 ; i < n ; ++i)
+	{
+		s[i + 1] = s[i] + v[i];
+		b[i + 1] = max(b[i], s[i + 1]);
 	}
 
-	for(int i=0;i<n;++i){
-		s[i+1]=s[i]+v[i];
-		b[i+1]=max(b[i], s[i+1]);
+	for(int i = 0 ; i <= n ; ++i) { ans = max(ans, b[i] + s[n] - s[i]); }
+
+	int t = 0;
+
+	for(int i = 0 ; i < n ; ++i)
+	{
+		t = max(t + v[i], 0);
+		ans = max(ans, t);
 	}
 
-	for(int i=0;i<=n;++i){
-		ans=max(ans, b[i]+s[n]-s[i]);
-	}
-	int t=0;
-	for(int i=0;i<n;++i){
-		t=max(t+v[i], 0);
-		ans=max(ans, t);
-	}
-
-	printf("%d\n", ans);
-	return 0;
+	printf("%d\n", ans);  return 0;
 }
