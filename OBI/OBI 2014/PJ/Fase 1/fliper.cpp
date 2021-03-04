70904f9a744bdfdf521206da02bbac10e0441b3b
Samyra Almeida <samyraalmeida@alu.ufc.br>
2021-03-04 09:24:28 -0300

    Update fliper.cpp

--- OBI/OBI 2014/PJ/Fase 1/fliper.cpp
+++ OBI/OBI 2014/PJ/Fase 1/fliper.cpp
@@ -1,23 +1,27 @@
-// Fliper - OBI 2014
-// By Samyra Almeida
+/*
+	OBI 2014 - Programação Nível Júnior - Fase 1 - Flíper
+	
+	Assunto: Estruturas de Decisão
+
+	Solução por Samyra Almeida
+*/
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int p, r;
 
-int main(){
+int main()
+{
     scanf("%d%d", &p, &r);
 
-    if(p==0 && (r==0 || r==1)){
-        printf("C\n");
-    }else{
-        if(r==0){
-            printf("B\n");
-        }
-        else{
-            printf("A\n");
-        }
+    if(p == 0 && (r == 0 || r == 1)) { printf("C\n"); }
+    else
+    {
+        if(r == 0) { printf("B\n"); }
+        else { printf("A\n"); }
     }
+
     return 0;
 }
